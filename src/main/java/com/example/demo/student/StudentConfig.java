8163261fe9f3ca83d02cc737461acffbf7b94df2
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2022-12-29 17:13:54 +0300

    Add database config

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student fatih = new Student(
+                    1L,
+                    "Fatih",
+                    LocalDate.of(2000, Month.FEBRUARY, 23),
+                    "fatih@gmail.com",
+                    22
+            );
+
+            Student zeynep = new Student(
+                    2L,
+                    "Zeynep",
+                    LocalDate.of(2003, Month.AUGUST, 16),
+                    "zeynep@gmail.com",
+                    22
+            );
+
+            repository.saveAll(
+                    List.of(fatih, zeynep)
+            );
+        };
+    }
+}
